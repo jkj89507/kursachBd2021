5963bd65675d3da7741a4af36780978861f2b23a
jkj89507 <jkj89507@gmail.com>
2021-01-13 09:00:59 +0300

    Add MOOORE USERS INFINITE POWER

--- pullinfo.py
+++ pullinfo.py
@@ -21,7 +21,6 @@ decrypted_text = cipher.decrypt(text).decode('utf-8') #no_serif
 print(decrypted_text)
 23423Gewfe-
 
-
 '''
 '''with open('data_login.csv', 'r') as csvfile:
     spamreader = csv.reader(csvfile)
@@ -30,6 +29,9 @@ print(decrypted_text)
         arr = row[0].split(";")
         admin.createElTable("data_login", (arr[0], hashlib.pbkdf2_hmac('sha256', arr[1].encode(), salt, 100000).hex(), i)) #data_login.csv
         i += 1
+        print('data_login: ',i)
+
+        #owner@owner.com;seHut56aegvd
 
 with open('bank_info.csv', 'r') as csvfile:
     spamreader = csv.reader(csvfile)
@@ -39,21 +41,30 @@ with open('bank_info.csv', 'r') as csvfile:
         text = cipher.encrypt(bytes(arr[0], 'utf-8'))
         admin.createElTable("bank_info", (i, text.decode("utf-8"), int(arr[1]))) #bank_info.csv
         i += 1
+        print('bank_info: ',i)
+
+        #1111-1111-1111-1111;56789
 
 with open('info_user.csv', 'r') as csvfile:
     spamreader = csv.reader(csvfile)
     i = 1
     for row in spamreader:
         arr = row[0].split(";")
-        admin.createElTable("info_user", (i ,arr[0], arr[1], arr[2], 'user')) #info_work.csv
+        admin.createElTable("info_user", (i ,arr[0], arr[1], arr[2], 'user')) #user.csv
         i += 1
+        print('info_user: ',i)
+
+        #Sergey Davidov;RF Mari El Volkova apt.149;Mon Aug 27 1989 21:33:33
 
 with open('info_work.csv', 'r') as csvfile:
     spamreader = csv.reader(csvfile)
+    i = 1
     for row in spamreader:
         arr = row[0].split(";")
         admin.createElTable("info_work", (arr[0], arr[1], arr[2])) #info_work.csv
-
+        i += 1
+        print('info_work: ',i)
+        #Sergey Davidov; 8(800)555-35-35;Gofast
 admin.updateElTable("info_user", "name='Sergey Davidov'", role="'"+"owner"+"'")'''
 
 
